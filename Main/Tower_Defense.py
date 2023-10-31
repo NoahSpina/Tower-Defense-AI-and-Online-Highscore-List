e4943b561499e57f92681287bd90be8546fd8378
John Goceljak <43759565+jgoceljak@users.noreply.github.com>
2023-10-31 16:57:04 -0400

    waypoints from placeholder map json now save to world instance

--- Main/Tower_Defense.py
+++ Main/Tower_Defense.py
@@ -3,6 +3,7 @@ import constants as c
 from enemy import Enemy
 import os
 from world import World
+import json
 
 # get working directory 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -21,20 +22,19 @@ pg.display.set_caption("Tower Defense")
 map_image = pg.image.load("levels/map.png").convert_alpha()
 enemy_image = pg.image.load('Assets/Images/Enemy/enemy_1.png').convert_alpha()
 
+#load level json
+with open('levels/map.tmj') as file:
+    world_data = json.load(file)
+
 #create world
-world = World(map_image)
+world = World(world_data, map_image)
+world.process_data()
 
 #create groups
 enemy_group = pg.sprite.Group()
 
-waypoints = [
-    (100, 100),
-    (400, 200),
-    (400, 100),
-    (200, 300)
-]
 
-enemy = Enemy(waypoints, enemy_image)
+enemy = Enemy(world.waypoints, enemy_image)
 enemy_group.add(enemy)
 
 #game loop
@@ -46,7 +46,7 @@ while run:
     world.draw(screen)
 
     #draw enemy path
-    pg.draw.lines(screen, "grey0", False, waypoints)
+    pg.draw.lines(screen, "grey0", False, world.waypoints)
 
     #update groups
     enemy_group.update()
--- Main/world.py
+++ Main/world.py
@@ -1,8 +1,27 @@
 import pygame as pg
 
 class World():
-    def __init__(self, map_image):
+    def __init__(self, data, map_image):
+        self.waypoints = []
+        self.level_data = data
         self.image = map_image
+    
+
+    #get waypoints from json
+    def process_data(self):
+        for layer in self.level_data["layers"]:
+            if layer["name"] == "waypoints":
+                for object in layer ["objects"]:
+                    waypoint_data = object["polyline"]
+                    self.make_waypoints(waypoint_data)
+    
+
+    def make_waypoints(self, data):
+        for item in data:
+            x_cord = item.get('x')         
+            y_cord = item.get('y')      
+            self.waypoints.append((x_cord, y_cord))
+
 
     def draw(self, surface):
         surface.blit(self.image, (0,0))
